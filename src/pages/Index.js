1bf8d30d94ba23f99a6a0427fab803bcd91cdb6b
Ludvig <l.lundh@hotmail.com>
2020-08-23 20:17:58 +0200

    add auth server and basic functionality requests

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,14 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import styled from 'styled-components'
 import { COLORS } from '../utils/colorUtils'
 import PlaylistPicker from '../components/Dropdown'
 import changeSelectedPlaylist from '../features/playlist/playlistSlice'
-import playlists from '../features/playlist/playlistMocks'
 
 const Container = styled.div`
   background-color: ${COLORS.spotify};
   height: 100%;
+  flex-direction: column;
 `
 
 const TitleContainer = styled.div`
@@ -19,7 +19,7 @@ const TitleContainer = styled.div`
 `
 
 const Title = styled.text`
-  color: ${props => props.theme.text.primary};
+  color: ${(props) => props.theme.text.primary};
   font-size: 39px;
 `
 
@@ -30,30 +30,192 @@ const PickerContainer = styled.div`
 `
 
 export default function Index() {
+  const [sessionError, setSessionError] = useState(false)
+  const [hashParams, setHashParams] = useState(null)
+  const [data, setData] = useState(null)
+  const [currentTrack, setCurrentTrack] = useState(null)
+  const [currentTrackUri, setCurrentTrackUri] = useState(null)
+  const BASE_URL = 'https://api.spotify.com/v1'
+
   const dispatch = useDispatch()
   function handlePlaylistSelected(playlist) {
     dispatch(changeSelectedPlaylist(playlist.label))
   }
 
+  function makeRequest(requestData, type) {
+    try {
+      let data
+      fetch(requestData.url, {
+        method: type,
+        headers: {
+          Authorization: 'Bearer ' + hashParams.access_token,
+        },
+      })
+        .then((res) => res.json())
+        .then((result) => {
+          console.log('result', result)
+          setData(data)
+        })
+    } catch (err) {
+      console.log('err', err)
+    }
+  }
 
-const statex = useSelector(state => state)
+  async function getCurrentPlayingSong() {
+    const requestData = {
+      url: `${BASE_URL}/me/player/currently-playing`,
+    }
+    console.log('hashParams', hashParams)
+    if (!hashParams?.access_token) {
+      setSessionError(true)
+    }
 
-useEffect(() => {
-  console.log('state', statex)
-}, [statex])
+    try {
+      fetch(requestData.url, {
+        method: 'get',
+        headers: {
+          Authorization: 'Bearer ' + hashParams.access_token,
+        },
+      })
+        .then((res) => res.json())
+        .then((result) => {
+          console.log('result', result)
+          setCurrentTrack(
+            `${result?.item?.name} - ${result?.item?.artists[0]?.name}`,
+          )
+          setCurrentTrackUri(result?.item.uri)
+        })
+    } catch (err) {
+      console.log('err', err)
+      setCurrentTrack('An error occured')
+    }
+  }
+
+  function handleAddCurentSongToPlaylist() {
+    const playlistId = '5LhDuj8sHVyR6H2k2RESCv'
+
+    const requestData = {
+      url: `${BASE_URL}/playlists/${playlistId}/tracks`,
+    }
+
+    const body = {
+      uris: [
+        {
+          currentTrackUri,
+        },
+      ],
+    }
+
+    console.log('body', body)
+    try {
+      fetch(requestData.url + `?uris=${currentTrackUri}`, {
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer ' + hashParams.access_token,
+        },
+        'Content-Type': 'application/json',
+      })
+        .then((res) => res.json())
+        .then((result) => {
+          console.log('result', result)
+        })
+    } catch (err) {
+      console.log('err', err)
+    }
+  }
+
+  function getMyData() {
+    const requestData = {
+      url: `${BASE_URL}/me/playlists`,
+    }
+
+    try {
+      fetch(requestData.url, {
+        method: 'get',
+        headers: {
+          Authorization: 'Bearer ' + hashParams.access_token,
+        },
+      })
+        .then((res) => res.json())
+        .then((result) => {
+          console.log('result', result)
+        })
+    } catch (err) {
+      console.log('err', err)
+    }
+  }
+
+  const statex = useSelector((state) => state)
+
+  useEffect(() => {
+    console.log('state', statex)
+  }, [statex])
+
+  useEffect(() => {
+    function getHashParams() {
+      let hashParams = {}
+      let e,
+        r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1)
+      e = r.exec(q)
+      while (e) {
+        hashParams[e[1]] = decodeURIComponent(e[2])
+        e = r.exec(q)
+      }
+      setHashParams(hashParams)
+    }
+
+    getHashParams()
+  }, [])
+
+  useEffect(() => {
+    console.log('hashParams', hashParams)
+    if (!hashParams?.access_token) {
+      setSessionError(true)
+    }
+    setSessionError(false)
+    if (sessionError) return
+
+    getCurrentPlayingSong()
+  }, [getCurrentPlayingSong, hashParams, sessionError])
 
   return (
     <Container>
       <TitleContainer>
         <Title>Quickify</Title>
       </TitleContainer>
-      <PickerContainer>
+      {sessionError && (
+        <TitleContainer>
+          <a href="http://localhost:8888">Login to Spotify</a>
+        </TitleContainer>
+      )}
+      {!sessionError && (
+        <>
+          <TitleContainer>
+            <button onClick={getCurrentPlayingSong}>
+              <text>Get current song</text>
+            </button>
+          </TitleContainer>
+          <TitleContainer>
+            <text>{currentTrack}</text>
+          </TitleContainer>
+          <TitleContainer>
+            <button onClick={handleAddCurentSongToPlaylist}>
+              Add current song to your chosen playlist
+            </button>
+          </TitleContainer>
+          <TitleContainer>
+            <button onClick={getMyData}>GetMyData</button>
+          </TitleContainer>
+        </>
+      )}
+      {/* <PickerContainer>
         <PlaylistPicker
-          items={playlists}
+          items={playlists}§
           onChange={handlePlaylistSelected}
           placeholder="Choose a playlist"
         />
-      </PickerContainer>
+      </PickerContainer> */}
     </Container>
   )
 }
