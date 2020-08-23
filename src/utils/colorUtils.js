13399f533dae0c85cd4afdf9878a99d47d93c8e5
Ludvig <l.lundh@hotmail.com>
2020-08-23 09:45:31 +0200

    add colors

--- src/utils/colorUtils.js
+++ src/utils/colorUtils.js
@@ -1,14 +1,20 @@
 export const COLORS = {
   white: '#ffffff',
-  black: '#000000',
+  black: '#191414',
   nero: '#222222',
+  spotify: '#1DB954',
 }
 
 export const LIGHT_THEME = {
-  background: COLORS.white
+  background: COLORS.white,
+  text: {
+    primary: COLORS.black,
+  },
 }
 
 export const DARK_THEME = {
-  background: COLORS.nero
+  background: COLORS.nero,
+  text: {
+    primary: COLORS.white,
+  },
 }
-
