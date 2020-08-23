6d87977e284b5c313a90d30d7af0bf2824794325
Ludvig <l.lundh@hotmail.com>
2020-08-23 09:46:11 +0200

    setup main container

--- src/pages/App.js
+++ src/pages/App.js
@@ -4,13 +4,21 @@ import withRedux from "../lib/withRedux";
 
 import Index from "./Index";
 import ThemeProvider from "../services/theme/ThemeProvider";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.background};
+  height: 100vh;
+`;
 
 function App({ reduxStore }) {
   console.log("Component", reduxStore);
   return (
     <Provider store={reduxStore}>
       <ThemeProvider>
-        <Index />
+        <Container>
+          <Index />
+        </Container>
       </ThemeProvider>
     </Provider>
   );
