b2b282e768887df2751ca1a3e411a1d0f836d5fa
Ludvig Lundh <ludvig.lundh@ancon.se>
2020-08-29 22:27:02 +0000

    add redux persist dep

--- src/features/app/rootReducer.js
+++ src/features/app/rootReducer.js
@@ -3,10 +3,12 @@ import themeReducer from '../theme/themeSlice'
 import accountReducer from '../account/accountSlice'
 import authReducer from '../auth/authSlice'
 import playlistReducer from '../playlist/playlistSlice'
+// import persistReducer from '../storage/storageSlice'
 
 export default combineReducers({
   theme: themeReducer,
   account: accountReducer,
   auth: authReducer,
   playlist: playlistReducer,
+  // persisit: persistReducer,
 })
--- src/features/app/store.js
+++ src/features/app/store.js
@@ -1,11 +1,19 @@
-import {configureStore} from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import rootReducer from './rootReducer'
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export function initializeStore(preloadedState) {
-  return (
-    configureStore({
-      reducer: rootReducer,
-      preloadedState
-    })
-  )
+  return configureStore({
+    reducer: persistedReducer,
+    preloadedState,
+  })
 }
