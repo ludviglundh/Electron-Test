75a0df60664f087c3663f07cb02861d1ce87ba4b
Ludvig Lundh <ludvig.lundh@ancon.se>
2020-08-29 17:01:33 +0000

    update window size

--- public/electron.js
+++ public/electron.js
@@ -1,22 +1,22 @@
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
-const isDev = require("electron-is-dev");
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const isDev = require('electron-is-dev')
 
 function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 1200,
     webPreferences: {
       nodeIntegration: true,
     },
-  });
+  })
 
   win.loadURL(
     isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`,
-  );
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`,
+  )
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(createWindow)
