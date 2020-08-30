318041d6041e476796fcf6644d4c048b8e809447
Ludvig Lundh <ludvig.lundh@ancon.se>
2020-08-30 10:29:42 +0000

    add baseUrl to constants

--- src/features/app/constants.js
+++ src/features/app/constants.js
@@ -8,11 +8,7 @@ export const WINDOW_DIMENSIONS = {
 export const THEME_NAMES = {
   light: 'light',
   dark: 'dark',
-};
+}
 
 export const CLIENT_ID = '54232bc5456443da81ac5918d86338b0'
-
-
-
-
-
+export const BASE_URL = 'https://api.spotify.com/v1'
