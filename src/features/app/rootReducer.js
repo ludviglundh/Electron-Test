90a34b28c30aeba3f63605f1e19445e68e184145
Ludvig Lundh <ludvig.lundh@ancon.se>
2020-08-30 09:38:53 +0000

    remove accountSlice imports

--- src/features/app/rootReducer.js
+++ src/features/app/rootReducer.js
@@ -1,13 +1,11 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import themeReducer from '../theme/themeSlice'
-import accountReducer from '../account/accountSlice'
 import authReducer from '../auth/authSlice'
 import playlistReducer from '../playlist/playlistSlice'
 // import persistReducer from '../storage/storageSlice'
 
 export default combineReducers({
   theme: themeReducer,
-  account: accountReducer,
   auth: authReducer,
   playlist: playlistReducer,
   // persisit: persistReducer,
